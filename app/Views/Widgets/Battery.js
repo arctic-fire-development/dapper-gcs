1405f31acf07841803e9ac4eada66f7dd1a9b27f
Rayjan Wilson <rayjan.wilson@gmail.com>
2014-01-03 12:23:54 -0900

    made this widget just swap out the image used. i just hard coded the images directory for now. can revisit

--- app/Views/Widgets/Battery.js
+++ app/Views/Widgets/Battery.js
@@ -1,33 +1,45 @@
 define(['backbone', 'JST'], function(Backbone, template) {
 
-  var BatteryWidget = Backbone.View.extend({
+    var BatteryWidget = Backbone.View.extend({
 
-    el: '#batteryWidget',
-    template: template['app/Templates/batteryWidget'],
-    className: 'widget',
+        el: '#batteryWidget',
+        template: template['app/Templates/batteryWidget'],
+        className: 'widget',
 
-    initialize: function() {
+        initialize: function() {
+            _.bindAll(this);
+            this.model.on('change:current_battery', this.render, this);
+            this.model.on('change:voltage_battery', this.render, this);
+            this.model.on('change:battery_remaining', this.render, this);
+        },
 
-      _.bindAll(this);
-      this.model.on('change:current_battery', this.render);
-      this.model.on('change:voltage_battery', this.render);
-      this.model.on('change:battery_remaining', this.render);
+        render: function() {
+            this.$el.html(this.template({
+                icon: this.getIcon(),
+                battery_remaining: this.model.get('battery_remaining'),
+                voltage_battery: this.model.get('voltage_battery') / 1000,
+                current_battery: this.model.get('current_battery') / -100
+            }));
+        },
 
-    },
-
-    render: function() {
-
-      this.$el.html(this.template(
-        {
-          battery_remaining: this.model.get('battery_remaining'),
-          voltage_battery: this.model.get('voltage_battery') / 1000,
-          current_battery: this.model.get('current_battery') / -100
+        getIcon: function() {
+            if (!this.model.get('battery_remaining')) {
+                return "images/battery-empty.svg";
+            } else {
+                var battery_remaining = this.model.get('battery_remaining');
+                if (battery_remaining >= 90) {
+                    return "images/battery-green.svg";
+                } else if (battery_remaining >= 60) {
+                    return "images/battery-yellow.svg";
+                } else if (battery_remaining >= 30) {
+                    return "images/battery-red.svg";
+                } else {
+                    return "images/battery-empty.svg";
+                }
+            }
         }
-      ));
 
-    }
-
-  });
-  return BatteryWidget;
+    });
+    return BatteryWidget;
 
 });
