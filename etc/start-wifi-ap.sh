8111b69ff966d647eb8a7a9f2cd05dcad222a848
Rayjan Wilson <rayjan.wilson@gmail.com>
2015-05-07 10:37:55 -0800

    adds script to dump versions numbers we are interested in tracking

--- etc/start-wifi-ap.sh
+++ etc/start-wifi-ap.sh
@@ -2,7 +2,8 @@
 
 echo "stopping wifi client";
 systemctl stop wpa_supplicant.service;
-sleep 2;
+
 echo "starting wifi ap";
 systemctl start hostapd.service;
+
 echo "wifi ap is up";
--- etc/start-wifi-client.sh
+++ etc/start-wifi-client.sh
@@ -2,9 +2,9 @@
 
 echo "stopping wifi ap";
 systemctl stop hostapd.service;
-sleep 2;
+
 echo "starting wifi client";
 systemctl start wpa_supplicant.service;
-sleep 2;
+
 configure_edison --wifi && curl -4 icanhazip.com;
 echo "wifi client is up";
